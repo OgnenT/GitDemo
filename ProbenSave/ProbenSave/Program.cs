cec0f78afa10c6e915cc2d57f8b634c24fcd6a12
Ognen <60581236+OgnenT@users.noreply.github.com>
2020-03-05 18:27:20 +0100

    iyyh

---
+++ ProbenSave/ProbenSave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProbenSave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
