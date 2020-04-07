b05d9ff01fb5c054c0fd85119f2b4ce9b5c038d9
Ognen <60581236+OgnenT@users.noreply.github.com>
2020-04-07 14:50:49 +0200

    Test

---
+++ CodeOrginizer/CodeOrginizer/Entities/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeOrginizer.Enums;
+
+namespace CodeOrginizer.Entities
+{
+    public class Job
+    {
+        public string CompanyName { get; set; }
+        public Address Address { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
---
+++ CodeOrginizer/CodeOrginizer/Helpers/GenerateRandomSSN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeOrginizer.Entities;
+
+namespace CodeOrginizer.Helpers
+{
+    public class GenerateRandomSSN
+    {
+        public static long GenerateSSN()
+        {
+            Random rand = new Random();
+            return rand.Next(1000000, 9999999);
+        }
+    }
+}
